f0841979956e4db32f47df5bd00e73f79835286e
JudeLakkis <JudeLakkis123@gmail.com>
2020-12-08 17:08:59 +0100

    Day 4

--- 04/Part2.py
+++ 04/Part2.py
@@ -10,3 +10,13 @@ with open('in.txt') as f:
     data.append(''.join(collect[:-1]))
 
 fields = sorted('byr iyr eyr hgt hcl ecl pid'.split(' '))
+
+for passport in data:
+    passport = passport.split()
+    # Dealing with 'cid' values
+    for key in passport:
+        if key[:3] == 'cid':
+            passport.pop(passport.index(key))
+        if len(passport) == len(fields):
+            if key[:3] == 'byr' and (int(key[4:]) >= 1920 and int(key[4:]) <= 2002):
+                print(key)
